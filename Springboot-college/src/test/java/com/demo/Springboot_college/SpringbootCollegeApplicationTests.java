ce3275a10aa825953648df4249c2c77aceccaf7d
MEGHA RAJESH <83894072+MeghaRajesh@users.noreply.github.com>
2025-03-01 22:06:39 +0530

    Add files via upload

---
+++ Springboot-college/src/test/java/com/demo/Springboot_college/SpringbootCollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Springboot_college;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
